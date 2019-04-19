6e02f12e735e1a0e46679457c8604415ea56c4a8
YauheniRusinchuk <ruevgal@gmail.com>
2019-04-19 14:51:32 +0300

    add hash function for password room

--- src/apps/room/views.py
+++ src/apps/room/views.py
@@ -5,12 +5,13 @@ from src.forms.room.forms import FormRoom
 from src.models.room.models import Room
 
 class CreateRoom(CreateView):
+    queryset        = Room.objects.all()
     form_class      = FormRoom
-    success_url     = '/'
     template_name   = 'room/create.html'
 
     def form_valid(self, form):
         form.instance.founder   = self.request.user
+        print(form.cleaned_data)
         return super().form_valid(form)
 
 
--- src/forms/room/forms.py
+++ src/forms/room/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from src.models.room.models import Room
+from django.contrib.auth.hashers import make_password, check_password
+
+#
+# hashed_pwd = make_password("plain_text")
+# check_password("plain_text",hashed_pwd)
+
 
 class FormRoom(forms.ModelForm):
 
-    password = forms.CharField(widget=forms.PasswordInput(
-             attrs={'placeholder': 'password ...'}
-    ))
+    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model   = Room
@@ -15,7 +19,6 @@ class FormRoom(forms.ModelForm):
         ]
 
 
-
-    # name     = forms.CharField()
-    #
-    #name.widget.attrs.update({"placeholder" : "name room ..."})
+    def clean_password(self):
+        hashed_pswd = make_password(self.cleaned_data.get('password'))
+        return hashed_pswd
