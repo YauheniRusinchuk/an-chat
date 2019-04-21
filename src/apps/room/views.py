f21ddf61dc7419d71d0dd779e10042da6b3c901d
YauheniRusinchuk <ruevgal@gmail.com>
2019-04-21 07:29:53 +0300

    fixed slug

--- src/apps/room/views.py
+++ src/apps/room/views.py
@@ -2,7 +2,7 @@ from django.views.generic.edit import CreateView, DeleteView
 from django.views import View
 from django.http import Http404
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from src.forms.room.forms import FormRoom
 from src.models.room.models import Room
 from django.utils.text import slugify
@@ -37,19 +37,17 @@ class CreateRoom(CreateView):
 class CheckPassowrd(View):
 
     def get(self, request, *args, **kwargs):
-        room = Room.objects.get(pk=kwargs.get('pk'))
-        slug        = slugify(room.name)
-        if request.user in room.participants.all():
-            return redirect(reverse('main:room:detail_room', kwargs={'slug': slug, 'pk': room.pk}))
+        room = get_object_or_404(Room, pk=kwargs.get('pk'))
+        if request.user in room.participants.all() or request.user == room.founder:
+            return redirect(reverse('main:room:detail_room', kwargs={'slug': room.slug, 'pk': room.pk}))
         return render(request, 'room/check_password.html', {})
 
     def post(self, request, *args, **kwargs):
-        room        = Room.objects.get(pk=kwargs.get('pk'))
-        slug        = slugify(room.name)
+        room = get_object_or_404(Room, pk=kwargs.get('pk'))
         password    = request.POST.get('password')
         if check_password(password, room.password):
             room.participants.add(request.user)
-            return redirect(reverse('main:room:detail_room', kwargs={'slug': slug, 'pk': room.pk}))
+            return redirect(reverse('main:room:detail_room', kwargs={'slug': room.slug, 'pk': room.pk}))
         return redirect('main:main_page')
 
 
@@ -57,9 +55,9 @@ class CheckPassowrd(View):
 class DetailtRoom(View):
 
     def get(self, request, *args, **kwargs):
-        room = Room.objects.get(pk=kwargs.get('pk'))
-        slug = slugify(room.name)
+        #room = Room.objects.get(pk=kwargs.get('pk'))
+        room = get_object_or_404(Room, pk=kwargs.get('pk'))
         if self.request.user == room.founder or self.request.user in room.participants.all():
             return render(request, 'room/detail.html', {'room': room})
         else:
-            return redirect(reverse('main:room:checkpassword', kwargs={'slug': slug, 'pk': room.pk}))
+            return redirect(reverse('main:room:checkpassword', kwargs={'slug': room.slug, 'pk': room.pk}))
--- src/models/room/models.py
+++ src/models/room/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.shortcuts import reverse
+from django.db.models.signals import pre_save
 from django.utils.text import slugify
 
 
@@ -9,17 +10,16 @@ class Room(models.Model):
     founder         = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='founder_room')
     name            = models.CharField(max_length=255, blank=False)
     password        = models.CharField(max_length=255, blank=False)
+    slug            = models.SlugField()
     participants 	= models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='users_in_room')
     create_date     = models.DateTimeField(auto_now_add=True)
 
 
     def get_absolute_url(self):
-        slug = slugify(self.name)
-        return reverse('main:room:detail_room', kwargs={'slug': slug, 'pk': self.pk})
+        return reverse('main:room:detail_room', kwargs={'slug': self.slug, 'pk': self.pk})
 
     def get_delete_room(self):
-        slug = slugify(self.name)
-        return reverse('main:room:delete_room', kwargs={'slug': slug, 'pk': self.pk})
+        return reverse('main:room:delete_room', kwargs={'slug': self.slug, 'pk': self.pk})
 
 
     def __str__(self):
@@ -28,3 +28,12 @@ class Room(models.Model):
 
     class Meta:
         ordering = ['-create_date']
+
+
+
+def pre_save_room_receiver(sender, instance, *args, **kwargs):
+    slug = slugify(instance.name)
+    instance.slug = slug
+
+
+pre_save.connect(pre_save_room_receiver, sender=Room)
