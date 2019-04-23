94e441a2598824539c78a19992b859fafe1f82ac
YauheniRusinchuk <ruevgal@gmail.com>
2019-04-23 06:22:35 +0300

    add fixed docker

--- config/settings.py
+++ config/settings.py
@@ -130,11 +130,11 @@ STATICFILES_DIRS = [
 
 
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("localhost", 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("localhost", 6379)],
+#         },
+#     },
+# }
